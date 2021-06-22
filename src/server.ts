86f8e8b0b6617fe16e162ae69be3262a3448e971
taylorbyks <taylorbyks@hotmail.com>
2021-06-22 16:08:05 -0300

    Add connection

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
+import 'reflect-metadata'
 import express from 'express'
+import { router } from './routes'
+import './database'
 
 const app = express()
 
-app.get('/test', (request, response) => {
-  return response.send("Teste")
-})
+app.use(express.json())
 
-app.post('/test', (request, response) => {
-  return response.send("Teste")
-})
+app.use(router)
 
-app.listen(3000, () => console.log("Server is running"))
+app.listen(3000, () => console.log('Server is running'))
