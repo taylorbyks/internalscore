d39a59a06639d7a82207eedd263527661f57f958
taylorbyks <taylorbyks@hotmail.com>
2021-06-23 11:41:26 -0300

    Add middleware

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
-import express from 'express'
+import 'express-async-errors'
+import express, { Request, Response, NextFunction } from 'express'
 import { router } from './routes'
 import './database'
 
@@ -8,5 +9,16 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if (error instanceof Error) {
+    return response.status(400).json({ error: error.message })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  })
+})
 
 app.listen(3000, () => console.log('Server is running'))
