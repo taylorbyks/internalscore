819de6f44dac959ec6c11a8e7928fdf35fc26ec2
taylorbyks <taylorbyks@hotmail.com>
2021-06-24 16:16:54 -0300

    Create compliments

---
+++ src/migration/1624557601994-CreateCompliments.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateCompliments1624557601994 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'user_sender',
+            type: 'uuid',
+          },
+          {
+            name: 'user_reciver',
+            type: 'uuid',
+          },
+          {
+            name: 'tag_id',
+            type: 'uuid',
+          },
+          {
+            name: 'message',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
